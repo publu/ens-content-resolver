37fcdc9431c1d175ff586754af12a4318e6e5d16
Pablo <pablo@hashingsystems.com>
2020-02-15 23:44:39 -0700

    Added Unstoppable Domains
    include resolver for .zil

--- src/js/resolver.js
+++ src/js/resolver.js
@@ -2,8 +2,15 @@ const Web3 = require('web3')
 const namehash = require('eth-ens-namehash')
 const multihash = require('multihashes')
 
+const {default: Resolution} = require('@unstoppabledomains/resolution')
+
 const REGISTRAR_MAIN_NET = "0x314159265dd8dbb310642f98f50c066173c1259b"
 const REGISTRAR_ROPSTEN = "0x112234455c3a32fd11230c42e7bccd4a84e02010"
+
+const resolution = new Resolution({blockchain: {
+  ens: {url: 'https://mainnet.infura.io/v3/YOUR-PROJECT-ID'},
+  cns: {url: 'https://mainnet.infura.io/v3/YOUR-PROJECT-ID'}
+}})
 
 var web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/0pzfHdAhsqakqtBk8Hs6"))
 
@@ -13,33 +20,51 @@ var abi = {
 }
 
 module.exports.resolve = function(name) {
-
-  let hash = namehash.hash(name)
-  Registrar = new web3.eth.Contract(abi.registrar, REGISTRAR_MAIN_NET)
-
-  return new Promise((resolve, reject) => {
-    Registrar.methods.resolver(hash).call()
-    .then((address) => {
-      if (address === '0x0000000000000000000000000000000000000000') {
-        reject(null)
-      } else {
-        Resolver = new web3.eth.Contract(abi.resolver, address)
-        return Resolver.methods.content(hash).call()
-      }
-    })
+  if(name.includes(".zil")) {
+    resolution.address(name, "ZIL")
+    .then(address => console.log(name, ' resolves to', address))
     .then((contentHash) => {
-      console.log("Content hash: " + contentHash)
+            console.log("Content hash: " + contentHash)
+            if (contentHash) {
+              // Remove 0x prefix
+              hex = contentHash.substring(2)
+              // Convert to buffer
+              buf = multihash.fromHexString(hex)
+              // Multihash encode and convert to base58
+              resolve(multihash.toB58String(multihash.encode(buf, 'sha2-256')))
+            } else {
+              reject('fisk')
+            }
+          })
 
-      if (contentHash) {
-        // Remove 0x prefix
-        hex = contentHash.substring(2)
-        // Convert to buffer
-        buf = multihash.fromHexString(hex)
-        // Multihash encode and convert to base58
-        resolve(multihash.toB58String(multihash.encode(buf, 'sha2-256')))
-      } else {
-        reject('fisk')
-      }
-    })
-  })
+  }else{
+        let hash = namehash.hash(name)
+        Registrar = new web3.eth.Contract(abi.registrar, REGISTRAR_MAIN_NET)
+
+        return new Promise((resolve, reject) => {
+          Registrar.methods.resolver(hash).call()
+          .then((address) => {
+            if (address === '0x0000000000000000000000000000000000000000') {
+              reject(null)
+            } else {
+              Resolver = new web3.eth.Contract(abi.resolver, address)
+              return Resolver.methods.content(hash).call()
+            }
+          })
+          .then((contentHash) => {
+            console.log("Content hash: " + contentHash)
+
+            if (contentHash) {
+              // Remove 0x prefix
+              hex = contentHash.substring(2)
+              // Convert to buffer
+              buf = multihash.fromHexString(hex)
+              // Multihash encode and convert to base58
+              resolve(multihash.toB58String(multihash.encode(buf, 'sha2-256')))
+            } else {
+              reject('fisk')
+            }
+          })
+        })                
+  }
 }
